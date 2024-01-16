df04f324f0852e7fbed244a07a83445edec3b4b7
JoshuaKar <joshuakarisch@gmail.com>
2024-01-16 17:34:50 -0600

    Apache SSL Cert

--- resumesite/resumesite/settings.py
+++ resumesite/resumesite/settings.py
@@ -27,9 +27,8 @@ with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['joshuakarischdev.net', '3.20.76.58']
 
-SSL_ENABLED = True
 # Application definition
 
 INSTALLED_APPS = [
